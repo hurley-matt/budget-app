4de9c2ee77289e9abd6ba0e781e84034ecbbe030
Matt Hurley <hurley04155@gmail.com>
2025-06-17 16:37:36 -0400

    Initial JavaScript commit

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 "use strict";
 import "./styles.css";
+
+const calculateBtn = document.getElementById("calculate");
+const monthlyIncome = document.getElementById("income");
+const housing = document.getElementById("houseing");
+const utilities = document.getElementById("utilities");
+const transportation = document.getElementById("transportation");
+const subscriptions = document.getElementById("subscriptions");
+const entertainment = document.getElementById("entertainment");
+const other = document.getElementById("other");
+
+calculateBtn.addEventListener("click");
+
+function budgetCalculation() {}
